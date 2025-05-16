3e8a8f38c51719821a1505fc5fc360b1001bdcfa
Skillter <57605954+Skillter@users.noreply.github.com>
2025-05-17 01:47:35 +0200

    Register a listener for the throwing mechanic

--- src/main/java/dev/skillter/blockgravity/listener/GravityGunListener.java
+++ src/main/java/dev/skillter/blockgravity/listener/GravityGunListener.java
@@ -6,30 +6,61 @@ import dev.skillter.blockgravity.permission.PermissionsEnum;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
-
-import java.io.StringWriter;
-import java.util.StringJoiner;
 
 public class GravityGunListener implements Listener {
 
     @EventHandler
-    public void playerUsedGravityGun(PlayerInteractEvent event) {
-
+    public void playerDelayedUseGravityGun(PlayerInteractEvent event) {
         if (event.getHand() != null && (event.getHand() != EquipmentSlot.OFF_HAND)
                 && event.getItem() != null && event.getItem().getType().equals(Material.STICK)
                 && event.getPlayer().hasPermission(PermissionsEnum.GRAVITY_GUN.getPermission())) {
+
 //            Bukkit.broadcastMessage(event.getItem().toString() + event.getItem().getType().name() + event.getHand().name() + event.getAction().name() + event.getHandlers().toString() + event.getMaterial());
 //            Bukkit.broadcastMessage(event.getHand().name());
             Bukkit.getScheduler().scheduleSyncDelayedTask(BlockGravity.INSTANCE, () -> {
-                GravityGunHandler.handle(event);
+                GravityGunHandler.useGravityGun(event);
             }, 1);
-
-
         }
     }
+
+    @EventHandler
+    public void playerSecondUseGravityGun(PlayerInteractEvent event) {
+        if (event.getHand() != null && (event.getHand() != EquipmentSlot.OFF_HAND)
+                && event.getItem() != null && event.getItem().getType().equals(Material.STICK)
+                && event.getPlayer().hasPermission(PermissionsEnum.GRAVITY_GUN.getPermission())
+                && (event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_BLOCK))) {
+
+            GravityGunHandler.throwBlock(event);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.HIGHEST)
+    public void playerSecondUseOnGravitatedBlockGravityGun(EntityDamageByEntityEvent event) {
+//        if (event.getEntity().getType().equals(EntityType.FALLING_BLOCK)
+//                && event.getDamager() instanceof Player
+//                && ((Player) event.getDamager()).getPlayer().getItemInUse() != null && ((Player) event.getDamager()).getPlayer().getItemInUse().getType().equals(Material.STICK)
+//                && ((Player) event.getDamager()).getPlayer().hasPermission(PermissionsEnum.GRAVITY_GUN.getPermission())) {
+//        ){
+        Bukkit.broadcastMessage("reaches");
+        GravityGunHandler.throwBlock(event);
+//        }
+    }
+
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.HIGHEST)
+    public void test(PlayerInteractEvent event) {
+        String msg = event.getEventName() + " " + event.getClass().getName() + event.getClass().descriptorString();
+        Bukkit.getLogger().info(msg);
+
+        HandlerList.getRegisteredListeners(0).
+    }
+
 
 }
